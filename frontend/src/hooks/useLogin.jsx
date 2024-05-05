ebfafcc2aac15f6c97b7ee404cb675a2763935fd
wahyuedy <wahedypras@gmail.com>
2024-05-05 14:12:37 +0700

    update frontend

--- frontend/src/hooks/useLogin.jsx
+++ frontend/src/hooks/useLogin.jsx
@@ -10,7 +10,7 @@ export const useLogin = () => {
         setIsLoading(true);
         setError(null);
         try {
-            const response = await fetch("http://localhost:4000/api/user/login", {
+            const response = await fetch(`${import.meta.env.VITE_URL_LOGIN}`, {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({ email, password })
